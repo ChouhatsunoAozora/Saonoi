b700c745eb03fb145d5c910af1c5d84531f409b7
ChouhatsunoAozora <6611110142@mut.ac.th>
2025-07-07 11:33:54 +0700

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ document.addEventListener("DOMContentLoaded", function () {
 });
 
 function tweetMessage() {
-    const message = "Saonoi Chouhatsu no Aozora\n#SaonoiBNK48\n#คำท้าทายที่ปลายฟ้าConceptDay1";
+    const message = "Saonoi ChouhatsunoAozora\n#SaonoiBNK48\n#คำท้าทายที่ปลายฟ้าConceptDay1";
     const tweetUrl = `https://twitter.com/intent/tweet?text=${encodeURIComponent(message)}`;
     window.open(tweetUrl, '_blank');
 }
